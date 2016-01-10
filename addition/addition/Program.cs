2a314c6ec0812f01d1b6657f70c3d07b0d5a20ce
dhanalakshmi <dlkpst@gmail.com>
2016-01-10 07:40:27 -0600

    I added addition file

---
+++ addition/addition/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int A=10;
+        int B=12;
+
+        int C = A + B;
+   Console.WriteLine(" Total Value=" +C);
+    Console.ReadLine();
+    }
+    }
+
